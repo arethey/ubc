db2a4f3cbde3d7c7c093ec7c2d2efc34d7ec4be8
Ardie C. Derrayal <31508884+arethey@users.noreply.github.com>
2018-06-05 19:12:24 +0800

    1

--- my-app/server.js
+++ my-app/server.js
@@ -14,20 +14,8 @@ app.use(bodyParser.json());
 app.use('/', appRoutes);
 http.createServer(app).listen(port);
 
-app.get('/', function (req, res){
-    // res.sendFile(__dirname + '/index.html');
-    // var cursor = db.collection('accountlist').find().toArray(function(err, results){
-    //     console.log(results);
-    // })
-});
 console.log('Listening to port:', port);
 
-app.post('/accountlist', function(req, res){
-    db.collection('accountlist').save(req.body, function(err, result){
-        if(err) return console.log(err);
-        console.log('saved to db');
-        res.redirect('/');
-    })
 app.get('*', prpl.makeHandler('.', {
   builds: [
     { name: '/', browserCapabilities: ['es2015', 'push'] },
